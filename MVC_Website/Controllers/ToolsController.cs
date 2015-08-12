e85b48be6138748ae70bba471d126cde7ba6939b
wcope80 <williamacope@gmail.com>
2015-08-11 21:34:45 -0500

    Commit Website into repo

---
+++ MVC_Website/Controllers/ToolsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Website
+{
+    public class ToolsController : Controller
+    {
+        // GET: Tools
+        public ActionResult MaxCalculator()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC_Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Website.Startup))]
+namespace MVC_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
